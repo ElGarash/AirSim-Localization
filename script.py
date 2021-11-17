ea53d9efefeb97944ecee10c8e41bde8abe98de2
Noor Eldeen Salah <nooreldeensalah99@gmail.com>
2021-11-17 11:07:17 +0200

    OOP Refactoring

--- script.py
+++ script.py
@@ -4,97 +4,115 @@ import airsim
 import click
 
 
-def destroy_stationery_vehicles(client):
-    """
-    Remove stationary vehicles from the environments
-    List of stationary vehicles objects exist in vehicles.txt
-    """
-    with open("vehicles.txt") as f:
-        vehicles = map(str.strip, f.readlines())
-        for vehicle in vehicles:
-            client.simDestroyObject(vehicle)
+class TrajectoryRecorder:
+    def __init__(self, aerial_pitch, ground_fov, aerial_altitude, ground_altitude):
+        self.client = airsim.MultirotorClient()
+        self.client.confirmConnection()
+        self.aerial_pitch = aerial_pitch
+        self.ground_fov = ground_fov
+        self.aerial_altitude = -aerial_altitude
+        self.ground_altitude = -ground_altitude
 
+    def destroy_stationery_vehicles(self):
+        """
+        Remove stationary vehicles from the environments
+        List of stationary vehicles objects exist in vehicles.txt
+        """
+        with open("vehicles.txt") as f:
+            vehicles = map(str.strip, f.readlines())
+            for vehicle in vehicles:
+                self.client.simDestroyObject(vehicle)
 
-def adjust_recording_parameters(client, aerial_pitch, ground_fov):
-    """
-    Customize recording parameters.
+    def adjust_recording_parameters(self):
+        """
+        Customize recording parameters.
 
-    Recommended parameters by 2020's AirSim paper are:
-    - aerial_pitch=50
-    - ground_fov=120
-    """
-    client.simSetCameraFov("front_center", ground_fov, "GroundDrone")
-    client.simSetCameraPose(
-        "bottom_center",
-        airsim.Pose(
-            orientation_val=airsim.to_quaternion(math.radians(aerial_pitch), 0, 0)
-        ),
-        "AerialDrone",
-    )
+        Recommended parameters by 2020's AirSim paper are:
+        - aerial_pitch=50
+        - ground_fov=120
+        """
+        self.client.simSetCameraFov("front_center", self.ground_fov, "GroundDrone")
+        self.client.simSetCameraPose(
+            "bottom_center",
+            airsim.Pose(
+                orientation_val=airsim.to_quaternion(
+                    math.radians(self.aerial_pitch), 0, 0
+                )
+            ),
+            "AerialDrone",
+        )
 
+    def initialize_drones(self):
+        """
+        Allow API control and taking off.
+        """
+        # Allow API Control
+        self.client.enableApiControl(True, "AerialDrone")
+        self.client.enableApiControl(True, "GroundDrone")
+        # Arm the vehicles (Not really sure what it means)
+        self.client.armDisarm(True, "AerialDrone")
+        self.client.armDisarm(True, "GroundDrone")
 
-def initialize_drones(client, aerial_altitude, ground_altitude):
-    """
-    Allow API control and taking off.
-    """
-    # Allow API Control
-    client.enableApiControl(True, "AerialDrone")
-    client.enableApiControl(True, "GroundDrone")
-    # Arm the vehicles (Not really sure what it means)
-    client.armDisarm(True, "AerialDrone")
-    client.armDisarm(True, "GroundDrone")
+        # Taking off
+        f1 = self.client.takeoffAsync(vehicle_name="AerialDrone")
+        f2 = self.client.takeoffAsync(vehicle_name="GroundDrone")
+        f1.join()
+        f2.join()
 
-    # Taking off
-    f1 = client.takeoffAsync(vehicle_name="AerialDrone")
-    f2 = client.takeoffAsync(vehicle_name="GroundDrone")
-    f1.join()
-    f2.join()
+        f1 = self.client.moveToZAsync(
+            z=self.aerial_altitude, velocity=5, vehicle_name="AerialDrone"
+        )
+        f2 = self.client.moveToZAsync(
+            z=self.ground_altitude, velocity=5, vehicle_name="GroundDrone"
+        )
+        f1.join()
+        f2.join()
 
-    f1 = client.moveToZAsync(z=aerial_altitude, velocity=5, vehicle_name="AerialDrone")
-    f2 = client.moveToZAsync(z=ground_altitude, velocity=5, vehicle_name="GroundDrone")
-    f1.join()
-    f2.join()
+        # Hovering (Not really sure what that means)
+        f1 = self.client.hoverAsync("AerialDrone")
+        f2 = self.client.hoverAsync("GroundDrone")
+        f1.join()
+        f2.join()
 
-    # Hovering (Not really sure what that means)
-    f1 = client.hoverAsync("AerialDrone")
-    f2 = client.hoverAsync("GroundDrone")
-    f1.join()
-    f2.join()
+    def traverse_and_record_trajectory(self):
+        # Read the trajectory trajectory_data into a list
+        trajectory_data = pandas.read_csv("trajectory.csv").values.tolist()
 
+        # Append the z coordinate to the xs and ys and convert the paths into airsim vectors
+        aerial_path = [
+            airsim.Vector3r(*position, self.aerial_altitude)
+            for position in trajectory_data
+        ]
+        ground_path = [
+            airsim.Vector3r(*position, self.ground_altitude)
+            for position in trajectory_data
+        ]
 
-def traverse_and_record_trajectory(client, aerial_altitude, ground_altitude):
-    # Read the trajectory trajectory_data into a list
-    trajectory_data = pandas.read_csv("trajectory.csv").values.tolist()
+        # Record and move on path
+        self.client.startRecording()
 
-    # Append the z coordinate to the xs and ys and convert the paths into airsim vectors
-    aerial_path = [airsim.Vector3r(*position, aerial_altitude) for position in trajectory_data]
-    ground_path = [airsim.Vector3r(*position, ground_altitude) for position in trajectory_data]
+        # Aerial drone
+        f1 = self.client.moveOnPathAsync(
+            aerial_path,
+            velocity=10,
+            drivetrain=airsim.DrivetrainType.ForwardOnly,
+            yaw_mode=airsim.YawMode(False),
+            vehicle_name="AerialDrone",
+        )
+        # Ground drone
+        f2 = self.client.moveOnPathAsync(
+            ground_path,
+            velocity=10,
+            drivetrain=airsim.DrivetrainType.ForwardOnly,
+            yaw_mode=airsim.YawMode(False),
+            vehicle_name="GroundDrone",
+        )
+        f1.join()
+        f2.join()
 
-    # Record and move on path
-    client.startRecording()
-
-    # Aerial drone
-    f1 = client.moveOnPathAsync(
-        aerial_path,
-        velocity=10,
-        drivetrain=airsim.DrivetrainType.ForwardOnly,
-        yaw_mode=airsim.YawMode(False),
-        vehicle_name="AerialDrone",
-    )
-    # Ground drone
-    f2 = client.moveOnPathAsync(
-        ground_path,
-        velocity=10,
-        drivetrain=airsim.DrivetrainType.ForwardOnly,
-        yaw_mode=airsim.YawMode(False),
-        vehicle_name="GroundDrone",
-    )
-    f1.join()
-    f2.join()
-
-    # Stop recording and reset
-    client.stopRecording()
-    client.reset()
+        # Stop recording and reset
+        self.client.stopRecording()
+        self.client.reset()
 
 
 @click.command()
@@ -127,13 +145,15 @@ def main(aerial_pitch, ground_fov, aerial_altitude, ground_altitude):
     A script used to collect ground and aerial views for a scene using drones in AirSim.
     """
     # Connect to the AirSim simulator
-    client = airsim.MultirotorClient()
-    client.confirmConnection()
 
-    destroy_stationery_vehicles(client)
-    adjust_recording_parameters(client, aerial_pitch, ground_fov)
-    initialize_drones(client, -aerial_altitude, -ground_altitude)
-    traverse_and_record_trajectory(client, -aerial_altitude, -ground_altitude)
+    trajectory_recorder = TrajectoryRecorder(
+        aerial_pitch, ground_fov, aerial_altitude, ground_altitude
+    )
+
+    trajectory_recorder.destroy_stationery_vehicles()
+    trajectory_recorder.adjust_recording_parameters()
+    trajectory_recorder.initialize_drones()
+    trajectory_recorder.traverse_and_record_trajectory()
 
 
 if __name__ == "__main__":
